0ac3af719ff2d96fc55379a3c073e7a28322e1b6
viktor <araujo>
2024-05-15 11:44:33 -0300

    Problem018: solution and tests

---
+++ problem_018/solution018.rb
@@ -0,0 +1,34 @@
+module DailyCodingProblem018
+  def self.balanced_brackets(str)
+    stack = []
+    process_characters(str, stack)
+    stack.empty?
+  end
+
+  def self.process_characters(str, stack)
+    str.each_char do |char|
+      if opening_bracket?(char)
+        stack.push(char)
+      elsif closing_bracket?(char)
+        return false if stack.empty? || stack.pop != matching_bracket(char)
+      end
+    end
+    true
+  end
+
+  def self.opening_bracket?(char)
+    ['(', '{', '['].include?(char)
+  end
+
+  def self.closing_bracket?(char)
+    [')', '}', ']'].include?(char)
+  end
+
+  def self.matching_bracket(char)
+    { ')' => '(', '}' => '{', ']' => '[' }[char]
+  end
+end
+
+puts DailyCodingProblem018.balanced_brackets("([])[]({})")
+puts DailyCodingProblem018.balanced_brackets("([)]")
+puts DailyCodingProblem018.balanced_brackets("((()") 
